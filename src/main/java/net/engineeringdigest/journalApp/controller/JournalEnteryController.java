df10361bc89c6f7493d2ced0cfa0db32194e4cb6
piyyushyadav <gamersrush9@gmail.com>
2026-07-22 05:09:23 +0530

    Created REST APIs

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEnteryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEnteryController.java
@@ -1,18 +1,27 @@
 package net.engineeringdigest.journalApp.controller;
 
-
-import net.engineeringdigest.journalApp.controller.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-public class JournalEnteryController {
+@RequestMapping("/journal")
+public class JournalEntryController {
 
-    private Map<Long, JournalEntry>  journalEntries = new HashMap<>();
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
-     public List<JournalEntry> getAll(){
-     return  new ArrayList<>(journalEntries.values());
+    @GetMapping("/abc")
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
 
-     }
+    @PostMapping
+     
 }
